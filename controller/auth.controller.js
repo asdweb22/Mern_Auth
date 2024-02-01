5e66918b4c56be49fba979766c59057682493751
asdweb22 <akshaydhongade548@gmail.com>
2024-02-01 13:13:59 +0530

    added middleware to handle error

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 
-export const Signup = async(req,res)=>{
+export const Signup = async(req,res,next)=>{
     try {
         const {username,email,password}=req.body
         const hashedPass=bcryptjs.hashSync(password,10)
         const newUser=new User({username,email,password:hashedPass})
         await newUser.save()
-        res.status(201).send({message:"New User created successfully"})
+        res.status(201).send({sucess:true,message:"New User created successfully",newUser})
     
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
 }
--- index.js
+++ index.js
@@ -17,7 +17,18 @@ app.use(express.json())
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
 
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "internal server error";
+    return res.status(statusCode).json({
+        success:false,
+        error:message,
+        statusCode:statusCode
+    })
+})
+
 const PORT=3000 
 app.listen(PORT,()=>{
     console.log(`server is runnung on http://localhost:${PORT}`)
 })
+
